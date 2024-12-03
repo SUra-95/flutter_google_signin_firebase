37dc6639f87821e1ed5294f13cdac139ebbe4c1d
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-12-04 01:33:52 +0530

    first commit

---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth.authStateChanges().listen((event) {
+      setState(() {
+        _user = event;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Auth with Fsasirebase'),
+      ),
+      body: _user != null ? _userInfo() : _googleSignInButton(),
+    );
+  }
+
+  Widget _googleSignInButton() {
+    return Center(
+      child: SizedBox(
+        height: 50,
+        child: SignInButton(Buttons.google, text: "Sign in with Google", onPressed: _handleGoogleSignIn),
+      ),
+    );
+  }
+
+  Widget _userInfo() {
+    print('user: $_user');
+    print('user id: ${_user?.uid}');
+    return SizedBox();
+  }
+
+  void _handleGoogleSignIn() {
+    try {
+      GoogleAuthProvider _googleAuthProvider = GoogleAuthProvider();
+      _auth.signInWithProvider(_googleAuthProvider);
+    } catch (error) {
+      print(error);
+    }
+  }
+}
